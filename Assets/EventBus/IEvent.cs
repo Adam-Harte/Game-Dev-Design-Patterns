a3b75f1b40ef2977b55fa64c0f6ee5aaa7f3dbb3
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-11 15:52:25 +0100

    Event Bus pattern docs update

--- Assets/EventBus/IEvent.cs
+++ Assets/EventBus/IEvent.cs
@@ -1,3 +1,6 @@
+// Empty interface used as a marker type which basically types certain classes that use the interface
+// so we know which classes will provide events
+
 public interface IEvent
 {
 
